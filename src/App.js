3b5e41223e189e195fd40f3b76a48e0910df9f2c
Ashna <ashnarys1320@gmail.com>
2022-06-02 00:00:22 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+function App(){
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+ 
+    <div class="banner">
+      <div class="navbar">
+        <img src="rose.jpg" class="logo"></img>
+        <ul>
+
+          <li><a href="https://twitter.com/ashnat__?t=D0bvum4O9W6dqqD1MjAKDA&s=09">Twitter</a></li>
+           <li><a href="https://instagram.com/ashnat___?igshid=YmMyMTA2M2Y=">Instagram</a></li>
+           <li><a href="https://www.linkedin.com/in/ashnat">Linkedin</a></li>
+           <li><a href="https://ashnaoffical.wixsite.com/portfolio">Site</a></li>
+
+        </ul>
+
+      </div>
+      <div class="content">
+        <h1>Hey there ,I am Ashna T</h1>
+        <p>Drop me a like</p>
+      </div>
+      <div>
+        <button type="button">Like</button>
+      </div>
+
     </div>
-  );
+    
+  )
 }
 
 export default App;
